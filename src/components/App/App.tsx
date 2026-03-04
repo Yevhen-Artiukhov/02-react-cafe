233fc94d508bad4ce3290f97c24c10d7637676cb
Yevhen <Artuikhov@gmail.com>
2026-03-04 20:56:18 +0200

    fixed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import css from "./App.module.css";
 
-import CafeInfo from "./components/CafeInfo/CafeInfo";
-import VoteOptions from "./components/VoteOptions/VoteOptions";
-import VoteStats from "./components/VoteStats/VoteStats";
-import Notification from "./components/Notification/Notification";
-
-import type { VoteType, Votes } from "./types/votes";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+import type { VoteType, Votes } from "../../types/votes";
 
 const initialVotes: Votes = {
   good: 0,
